8412dc36c531706034e1eb3ea4b3a0c296f5bf06
RaulAG-Dev <a24216890@alumnos.uady.mx>
2025-09-02 18:12:11 +0000

    Nueva linea

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -5,6 +5,10 @@ public class Main {
         Cliente cliente1= new Cliente("Eduardo",23874328);
         CuentaBancaria cuenta1 = new CuentaBancaria(2834634,cliente1,10000);
 
+        Cliente cliente2 = new Cliente("LUis Goméz",2384973);
+
+        CuentaBancaria cuenta2 = new CuentaBancaria(123,new Cliente("Juan Gomez",2343523),12000);
+
     }
 
 }
